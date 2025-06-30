9095603818b043153c4194acf5d081070a821908
Alex Inga <alexinga98@gmail.com>
2025-06-30 10:39:51 -0400

    changed to switch api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,10 @@
+import { secretApiKey } from './environment.secret';
+
 export const headers = {
-  apikey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnhjZm9ncGtoem55Z3NhdWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4OTkxMjQsImV4cCI6MjA2NTQ3NTEyNH0.7qGuizsQ9h3Js6OI0hDH1Ojp5CS7xwZYt7lvbrUIfrc',
+  apikey: secretApiKey,
 };
-// export const headers = {
-//   apikey:
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnhjZm9ncGtoem55Z3NhdWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4OTkxMjQsImV4cCI6MjA2NTQ3NTEyNH0.7qGuizsQ9h3Js6OI0hDH1Ojp5CS7xwZYt7lvbrUIfrc',
-//   Authorization:
-//     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnhjZm9ncGtoem55Z3NhdWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4OTkxMjQsImV4cCI6MjA2NTQ3NTEyNH0.7qGuizsQ9h3Js6OI0hDH1Ojp5CS7xwZYt7lvbrUIfrc',
-// };
 
 export const authHeaders = {
-  apikey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnhjZm9ncGtoem55Z3NhdWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4OTkxMjQsImV4cCI6MjA2NTQ3NTEyNH0.7qGuizsQ9h3Js6OI0hDH1Ojp5CS7xwZYt7lvbrUIfrc',
+  apikey: secretApiKey,
   'Content-Type': 'application/json',
 };
